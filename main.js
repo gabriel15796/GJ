df39540607b54a3a10e2c2c722f741ddc9e304e5
gabriel15796 <137406445+gabriel15796@users.noreply.github.com>
2023-10-26 09:18:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function tocaSomPom(){
+     document.querySelector('#som_tecla_pom').play('');
+}
+document.querySelector('.tecla_pom').onclick= tocaSomPom ();
+document.querySelectorAII('.tecla')
+//document.querySletor('.tecla_pom').onclick = toca
+const listaDeTeclas = document.querySelectorAII('tecla');
