2e11871154342270998d13820a2f6615265e4923
gabriel15796 <137406445+gabriel15796@users.noreply.github.com>
2023-11-09 09:34:16 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-function tocaSomPom(){
-     document.querySelector('#som_tecla_pom').play('');
+function tocaSomPom(seletrodorAduio){
+     document.querySelector(idElementoAudio).play('');
 }
 document.querySelector('.tecla_pom').onclick= tocaSomPom ();
 document.querySelectorAII('.tecla')
 //document.querySletor('.tecla_pom').onclick = toca
 const listaDeTeclas = document.querySelectorAII('tecla');
+while(contador<9){}
+listaDeTeclas[contador].classlist[1];
+listaDeTeclas[contador].onclick = function(){
+     tocaSom('#som_tecla_clap');
+     tocasom(idaudio);
+};
+while(contador < listaDeTeclas.length){
+     for (contador = 0; contador <listaDeTeclas.length){
+ };
+ tecla.onclick =function(){
+     tocaSom(idaudio);
+ };
+ tecla.onkeydown = function (evento){
+     if(evento.code ==='entre')
+     tecla.classlist.add('ativa');
+ };
+ tecla.onkeydown = function (){
+     tecla.classlist.remove('ativa');
+ };
+ tecla.onkeydown = function (evento){
+     if(evento.code===Space// evento.code ==='entre'){ 
+ }
